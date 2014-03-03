76b87ef99e6465b9ab8862c9d8eb808b1de4bf78
pkxiuluo <616197526@qq.com>
2014-03-03 16:30:21 +0800

    地图显示

--- LocationLib/src/com/example/locationlib/MapActivity.java
+++ LocationLib/src/com/example/locationlib/MapActivity.java
@@ -1,5 +1,6 @@
 package com.example.locationlib;
 
+import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 
 import android.os.Bundle;
@@ -8,12 +9,60 @@ import android.support.v4.app.FragmentActivity;
 public class MapActivity extends FragmentActivity {
 
 	private MapView mapView;
+	private AMap aMap;
 
 	@Override
-	protected void onCreate(Bundle arg0) {
-		super.onCreate(arg0);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		mapView = (MapView) findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);// 必须要写
+		init();
+	}
+
+	/**
+	 * 初始化AMap对象
+	 */
+	private void init() {
+		if (aMap == null) {
+			aMap = mapView.getMap();
+		}
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mapView.onPause();
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
 	}
 
 }
