50976703a549e5d011f9b968f8b4f9a1cb84552d
pkxiuluo <616197526@qq.com>
2014-03-07 17:38:50 +0800

    drawer

---
+++ HealthSLife/src/com/healthslife/adapters/NavigationAdapter.java
@@ -0,0 +1,57 @@
+package com.healthslife.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.healthslife.R;
+
+public class NavigationAdapter extends BaseAdapter {
+	private Context mContext;
+	private int[] titlesResId = { R.string.navi_run, R.string.navi_health_test, R.string.navi_invite,
+			R.string.navi_setting };
+
+	public NavigationAdapter(Context context) {
+		mContext = context;
+	}
+
+	@Override
+	public int getCount() {
+		return titlesResId.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.activity_main, null);
+			holder = new ViewHolder();
+			holder.img = (ImageView) convertView.findViewById(R.id.navi_icon_img);
+			holder.txt = (TextView) convertView.findViewById(R.id.navi_title_txt);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.txt.setText(titlesResId[position]);
+		return convertView;
+	}
+
+	private class ViewHolder {
+		ImageView img;
+		TextView txt;
+	}
+
+}
