9fa57ca72d99df9b413f02f2f07a04d731e78963
pkxiuluo <616197526@qq.com>
2014-03-07 17:38:30 +0800

    drawer

--- HealthSLife/gen/com/healthslife/R.java
+++ HealthSLife/gen/com/healthslife/R.java
@@ -24,22 +24,29 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int text_size_big=0x7f040002;
+        public static final int text_size_normal=0x7f040003;
+        public static final int text_size_small=0x7f040004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int loading_rotate=0x7f020001;
     }
     public static final class id {
-        public static final int action_compose=0x7f080004;
-        public static final int action_search=0x7f080003;
-        public static final int btn=0x7f080000;
-        public static final int img=0x7f080001;
-        public static final int map=0x7f080002;
+        public static final int action_compose=0x7f080007;
+        public static final int action_search=0x7f080006;
+        public static final int main_content_layout=0x7f080001;
+        public static final int main_drawer_layout=0x7f080000;
+        public static final int main_left_navi_list=0x7f080002;
+        public static final int map=0x7f080003;
+        public static final int navi_icon_img=0x7f080004;
+        public static final int navi_title_txt=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_map=0x7f030001;
         public static final int dialog_http_loading=0x7f030002;
+        public static final int item_navigation=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -48,6 +55,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int navi_health_test=0x7f050004;
+        public static final int navi_invite=0x7f050005;
+        public static final int navi_run=0x7f050003;
+        public static final int navi_setting=0x7f050006;
     }
     public static final class style {
         /** 
--- HealthSLife/src/com/healthslife/activitys/MainActivity.java
+++ HealthSLife/src/com/healthslife/activitys/MainActivity.java
@@ -1,62 +1,29 @@
 package com.healthslife.activitys;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.RotateDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ListView;
 
 import com.healthslife.BaseFragmentActivity;
 import com.healthslife.R;
-import com.healthslife.http.MyAsyncHttpResponseHandler;
-import com.loopj.android.http.AsyncHttpClient;
+import com.healthslife.adapters.NavigationAdapter;
 
 public class MainActivity extends BaseFragmentActivity {
-	public Button btn;
-	private ImageView img;
-	
-	public AsyncHttpClient mClient;
-	
-	public MyAsyncHttpResponseHandler handler;
+	private DrawerLayout drawerLayout;
+	private ListView navi;
+	private String[] title = { "1", "2", "3" };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		init();
-		
-		handler = new MyAsyncHttpResponseHandler(this, mClient) {
-			public void onSuccess(int arg0, org.apache.http.Header[] arg1, byte[] arg2) {
-				Bitmap bitmap = BitmapFactory.decodeByteArray(arg2, 0,
-						arg2.length);
-				img.setImageBitmap(bitmap);
-				
-				super.onSuccess(arg0, arg1, arg2);
-			};
-		};
 	}
 
 	private void init() {
-		mClient = new AsyncHttpClient();
-		img = (ImageView) findViewById(R.id.img);
-		btn = (Button) findViewById(R.id.btn);
-
-		btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mClient.get("http://www.baidu.com/img/bdlogo.gif", handler);
-			}
-
-		});
+		drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+		navi = (ListView) findViewById(R.id.main_left_navi_list);
+		navi.setAdapter(new NavigationAdapter(this));
 	}
 
 }
