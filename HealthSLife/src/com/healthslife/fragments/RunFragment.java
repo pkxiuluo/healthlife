21740113a15881f00a14f103289def761e5c59f1
pkxiuluo <616197526@qq.com>
2014-03-10 17:11:39 +0800

    改变tab方案 采用 PagerSlidingTabStrip方案

--- HealthSLife/src/com/healthslife/fragments/RunFragment.java
+++ HealthSLife/src/com/healthslife/fragments/RunFragment.java
@@ -3,14 +3,17 @@ package com.healthslife.fragments;
 import com.healthslife.R;
 
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-public class RunFragment extends Fragment {
+public class RunFragment extends Fragment   {
 	private ActionBar actionBar;
 	private FrameLayout contentLayout;
 
@@ -18,11 +21,7 @@ public class RunFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.fragment_run, container, false);
 		contentLayout = (FrameLayout) root.findViewById(R.id.run_content_layout);
-		actionBar = getActivity().getActionBar();
-		// Specify that we will be displaying tabs in the action bar.
-		// actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-//		actionBar.addTab(actionBar.newTab().setText(R.string.run_normal));
-//		actionBar.addTab(actionBar.newTab().setText(R.string.run_destination));
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
+
 }
