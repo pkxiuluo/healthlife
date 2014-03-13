78f57372e1bc552e91a8d933211bc86684930f1b
pkxiuluo <616197526@qq.com>
2014-03-13 17:39:27 +0800

    runClient

---
+++ HealthSLife/src/com/healthslife/run/RunClient.java
@@ -0,0 +1,17 @@
+package com.healthslife.run;
+
+public class RunClient {
+	public void setRunSetting(){
+		
+	}
+	
+	public void start(){
+	}
+	
+	public void stop(){
+	}
+
+	public interface OnStatusChangedListener{
+		public void 
+	}
+}
