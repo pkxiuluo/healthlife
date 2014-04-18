c3956e470e3b09fb3f3a86b2c1d79345dc00ab06
pkxiuluo <616197526@qq.com>
2014-04-18 12:04:42 +0800

    历史记录 下拉列表

--- HealthSLife/src/com/healthslife/activitys/RunHistoryActivity.java
+++ HealthSLife/src/com/healthslife/activitys/RunHistoryActivity.java
@@ -2,14 +2,42 @@ package com.healthslife.activitys;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListPopupWindow;
+import android.widget.TextView;
 
 import com.healthslife.BaseFragmentActivity;
 import com.healthslife.R;
-import com.healthslife.run.dao.RunSetting;
 
-public class RunHistoryActivity extends BaseFragmentActivity {
+public class RunHistoryActivity extends BaseFragmentActivity implements OnClickListener {
 	private ActionBar actionBar;
+	private Button dateDropBtn;
+	private Button patternDropBtn;
+	private Button completeDropBtn;
+
+	private ListPopupWindow dateLPW;
+	private ListPopupWindow patternLPW;
+	private ListPopupWindow completeLPW;
+	private MyAdapter dateAdapter;
+	private MyAdapter patternAdapter;
+	private MyAdapter completeAdapter;
+
+	private String[] dateStrings;
+	private String[] patternStrings;
+	private String[] completeStrings;
+	private int current_date = 0;
+	private int current_pattern = 0;
+	private int current_complete = 0;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -20,6 +48,25 @@ public class RunHistoryActivity extends BaseFragmentActivity {
 
 	private void initView() {
 		setContentView(R.layout.activity_run_history);
+		dateDropBtn = (Button) findViewById(R.id.run_history_date_drop_btn);
+		patternDropBtn = (Button) findViewById(R.id.run_history_pattern_drop_btn);
+		completeDropBtn = (Button) findViewById(R.id.run_history_complete_drop_btn);
+		dateDropBtn.setOnClickListener(this);
+		patternDropBtn.setOnClickListener(this);
+		completeDropBtn.setOnClickListener(this);
+		dateLPW = createListPopupWindow(dateDropBtn);
+		patternLPW = createListPopupWindow(patternDropBtn);
+		completeLPW = createListPopupWindow(completeDropBtn);
+		dateStrings = getResources().getStringArray(R.array.run_history_date_drop_list);
+		patternStrings = getResources().getStringArray(R.array.run_history_pattern_drop_list);
+		completeStrings = getResources().getStringArray(R.array.run_history_complete_drop_list);
+		dateAdapter = new MyAdapter(dateStrings);
+		patternAdapter = new MyAdapter(patternStrings);
+		completeAdapter = new MyAdapter(completeStrings);
+
+		dateLPW.setAdapter(dateAdapter);
+		patternLPW.setAdapter(patternAdapter);
+		completeLPW.setAdapter(completeAdapter);
 	}
 
 	private void setActionBar() {
@@ -39,4 +86,106 @@ public class RunHistoryActivity extends BaseFragmentActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onClick(View v) {
+		controlDropList(v);
+	}
+
+	private void controlDropList(View dropBtn) {
+		if (dropBtn == dateDropBtn) {
+			dateLPW.show();
+		} else {
+			dateLPW.dismiss();
+		}
+		if (dropBtn == patternDropBtn) {
+			patternLPW.show();
+		} else {
+			patternLPW.dismiss();
+		}
+		if (dropBtn == completeDropBtn) {
+			completeLPW.show();
+		} else {
+			completeLPW.dismiss();
+		}
+	}
+
+	private ListPopupWindow createListPopupWindow(View anchorView) {
+		ListPopupWindow LPW = new ListPopupWindow(this);
+		LPW.setAnchorView(anchorView);
+		LPW.setHeight(LayoutParams.WRAP_CONTENT);
+		LPW.setOnItemClickListener(new MyDropItemClick(anchorView));
+		return LPW;
+	}
+
+	private class MyAdapter extends BaseAdapter {
+		private String[] items;
+
+		public MyAdapter(String[] items) {
+			this.items = items;
+		}
+
+		@Override
+		public int getCount() {
+			if (items == null) {
+				return 0;
+			} else {
+				return items.length;
+			}
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return items[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView textView;
+			if (convertView == null) {
+				textView = new TextView(RunHistoryActivity.this);
+				textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.text_size_small_l));
+				textView.setGravity(Gravity.CENTER);
+				int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources()
+						.getDisplayMetrics());
+				textView.setPadding(0, margin, 0, margin);
+			} else {
+				textView = (TextView) convertView;
+			}
+			textView.setText(items[position]);
+			return textView;
+		}
+	}
+
+	private class MyDropItemClick implements OnItemClickListener {
+		View dropBtn;
+
+		MyDropItemClick(View view) {
+			dropBtn = view;
+		}
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			if (dropBtn == dateDropBtn) {
+				dateDropBtn.setText(dateStrings[position]);
+				dateLPW.dismiss();
+				current_date = position;
+			} else if (dropBtn == patternDropBtn) {
+				current_pattern = position;
+				patternLPW.dismiss();
+				patternDropBtn.setText(patternStrings[position]);
+			} else if (dropBtn == completeDropBtn) {
+				current_complete = position;
+				completeLPW.dismiss();
+				completeDropBtn.setText(completeStrings[position]);
+			}
+		}
+
+	}
+
 }
