b271a17a89b961edc465a0804b88c29fb44beea1
pkxiuluo <616197526@qq.com>
2014-05-20 14:32:00 +0800

    数据库修改

--- HealthSLife/src/com/healthslife/db/DataBaseHelper.java
+++ HealthSLife/src/com/healthslife/db/DataBaseHelper.java
@@ -2,12 +2,13 @@ package com.healthslife.db;
 
 import java.sql.SQLException;
 
-import android.R.integer;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.healthslife.healthtest.dao.HeartRateHisRecord;
 import com.healthslife.healthtest.dao.ECGAnalysisRecord;
+import com.healthslife.healthtest.dao.HeartRateHisRecord;
 import com.healthslife.run.dao.RunRecord;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
@@ -16,11 +17,13 @@ import com.j256.ormlite.table.TableUtils;
 
 public class DataBaseHelper extends OrmLiteSqliteOpenHelper {
 	private static final String DB_NAME = "healthlife.db";
-	private static final int DB_VSERSION = 1;
+	private static final int DB_VSERSION = 2;
 	private Dao<HeartRateHisRecord, Integer> heartRateDao = null;
+	private Context mContext;
 
 	public DataBaseHelper(Context context) {
 		super(context, DB_NAME, null, DB_VSERSION);
+		mContext = context;
 	}
 
 	@Override
@@ -37,7 +40,11 @@ public class DataBaseHelper extends OrmLiteSqliteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, ConnectionSource arg1, int arg2,
 			int arg3) {
-
+		try {
+			TableUtils.createTable(connectionSource, ECGAnalysisRecord.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
